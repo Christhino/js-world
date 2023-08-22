b6fa76b33a7c359b5bed5c9d282f4ca04638f4b7
christhino <mintsaniaina.christhino@gmail.com>
2023-08-22 10:14:15 +0300

    mise en page

--- components/layout/Why.tsx
+++ components/layout/Why.tsx
@@ -1,80 +1,26 @@
-import { tw } from 'twind';
-import Check from '@/app/image/check.svg';
-
-const posts = [
-    {
-      id: 1,
-      title: 'Boost your conversion rate',
-      href: '#',
-      description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    
-      category: { title: 'Innovation', href: '#' },
-    
-    },
-    {
-        id: 2,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-      
-        category: { title: 'Sécuriser', href: '#' },
-       
-      },
-      {
-        id: 3,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-      
-        category: { title: 'Assistance', href: '#' },
-      
-      },
-      {
-        id: 4,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-       
-        category: { title: 'Flexibilité et stabilité', href: '#' },
-       
-      },
-      {
-        id: 5,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        
-        category: { title: 'Une expertise métier', href: '#' },
-       
-      },
-      {
-        id: 6,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-          'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-     
-        category: { title: 'Accessibilité', href: '#' },
-       
-      },
-    // More posts...
-]
-  
+import Balancer from "react-wrap-balancer";
 export default function Why() {
     return (
       <section className="bg-white pb-6">
         <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
             <div className="container mx-auto px-6 p-6 bg-white">
                 <div className="mb-16 text-center">
-                  <h4 className="text-base text-indigo-600 font-semibold tracking-wide uppercase" >Pourquoi nous choisir? </h4>
-                  <p className="mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900">
-                    Pourquoi choisir la plateforme BESC  JLS  WORLD
-                  </p>
+                <a
+          href="https://twitter.com/steventey/status/1613928948915920896"
+          target="_blank"
+          rel="noreferrer"
+          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
+        >
+          <p className="text-base font-semibold text-[#1d9bf0]">
+           Pourquoi  nous choisir ?
+          </p>
+        </a>
+        <h1
+          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+        >
+          <Balancer> Pourquoi choisir la plateforme BESC JLS WORLD </Balancer>
+        </h1>
                 </div>
                 <div className="flex flex-wrap my-12">
                     <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8">
