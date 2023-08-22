d0d7cbaced38d2165ac64eeea1fce30ed162c13b
christhino <mintsaniaina.christhino@gmail.com>
2023-08-22 10:49:00 +0300

    fix

--- components/Actualite/SingleBlog.tsx
+++ components/Actualite/SingleBlog.tsx
@@ -11,8 +11,8 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
         data-wow-delay=".1s"
       >
         <Link href="/" className="relative block h-[220px] w-full">
-          <span className="absolute top-6 right-6 z-20 inline-flex items-center justify-center rounded-full bg-primary py-2 px-4 text-sm font-semibold capitalize text-white">
-            {tags[0]}
+          <span className="absolute top-6 right-6 z-20 inline-flex items-center justify-center rounded-full bg-[rgb(74,108,247)] py-2 px-4 text-sm font-semibold capitalize text-white">
+            {tags}
           </span>
           <Image src={image} alt="image" fill />
         </Link>
